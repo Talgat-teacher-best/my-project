43c46aeedb271207a597c762311eac3b17a57c85
Talgat-teacher-best <62733790+Talgat-teacher-best@users.noreply.github.com>
2020-11-14 15:06:02 +0500

    Home Work

---
+++ Nurda/Nurda/catalog/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
+
+admin.site.register(Genre)
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre', 'language1')
+    inlines = [BooksInstanceInline]
+
+
+# Register the Admin classes for BookInstance using the decorator
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+	
+    list_display = ('book', 'status', 'due_back', 'id', 'borrower')
+    list_filter = ('status', 'due_back',)
+    
+    fieldsets = (
+        (None, {
+            'fields': ('book','imprint', 'id',)
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
+
